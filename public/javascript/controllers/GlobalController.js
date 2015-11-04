b0f52a6a637bfc15ca71c43467c9160c9c1bdb75
ajahannes <aguerojahannes@gmail.com>
2015-11-03 19:43:30 -0800

    toasts, autosave

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -3,7 +3,7 @@
 	angular.module('app')
 	.controller('GlobalController', GlobalController);
 
-	function GlobalController(GlobalFactory, $timeout, $mdSidenav, $log, $state, $stateParams) {
+	function GlobalController(GlobalFactory, $timeout, $mdSidenav, $log, $state, $stateParams, $mdToast) {
 		var glob = this;
 		glob.sections = GlobalFactory.sections;
 		glob.user = {};
@@ -21,11 +21,22 @@
 			GlobalFactory.login(glob.user).then(function(res){
 				glob.user = {};
 				nav.errLog = "";
+				glob.showSimpleToast('Welcome!' + glob.status.username);
 				$state.go("root.Dashboard", {id: glob.status._id}); // tried something like this: root.Dashboard({$stateParams.id: glob.status._id}) and root.Dashboard", {$stateParams.id: glob.status._id} to go to that user. , {$stateParams.id}
 			}, function(err){
 				nav.errLog = err;
+				glob.showSimpleToast('Try again. Wrong username or password.!');
 			});
 		};
+
+		glob.showSimpleToast = function(content) {
+			  $mdToast.show(
+				 $mdToast.simple()
+				 .content(content)
+				 .position("top left")
+				 .hideDelay(2000)
+			  );
+			};
 
 // SIGN UP
 	glob.signUp = function() {
